cd6d1ad50c23812a677cad180ec5eaf685fa27f1
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-23 19:24:18 +0500

    javascript practise

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,48 +1,51 @@
 import './Cart.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 function Cart() {
-    const [getdata , setGetData]  = useState();
-    
-    const array = []
-    
-    const handleChange = (e)=>{
-        setGetData(e.target.value);
-       
-    }
+    const [getdata, setGetData] = useState("");
+    const [list, setList] = useState([]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        
-       array.unshift(...array,getdata)
-        setGetData(array)
+        console.log(getdata);
+        const data = { getdata }
+        setList((is) => [data, ...is])
         setGetData("")
-        console.warn(array)
-
-        
-          
     }
-    
-    
-    return (<form onClick={handleSubmit}  >
-        
-        
+
+
+
+    const deleteButton = (index) => {
+      const Data = [...list];
+      Data.slice(index,1)
+      setList(Data)
+    }
+
+
+
+    return (<form   >
+
+
         <div className="whole-div">
-        <div className="enter-value-div">
-        <input type="text" placeholder="Enter the value" className="enter-value-div" value={getdata} onChange={handleChange}/>
-        <div className="button-div">
-        <button >Add</button></div>
+            <div className="enter-value-div">
+                <input type="text" placeholder="Enter the value" className="enter-value-div" value={getdata} onChange={(e) => setGetData(e.target.value)} />
+                <div className="button-div">
+                    <button onClick={handleSubmit}>Add</button></div>
+            </div>
+            {list.map((a,index) => <div><li id={index}>
+            <div className="secound-outpot">
+                <span className="add-data-list" >{a.getdata}</span>{index}
+                <div className="button-div">
+                    <button  >Edit</button>
+                    <button onClick={()=>deleteButton(index)} >Delete</button></div>
+            </div></li></div>)}
         </div>
-         <div className="secound-outpot">
-         <span className="add-data-list" >{getdata}</span>
-         <div className="button-div">
-         <button onClick={} >Edit</button>
-          <button onClick={} >Delete</button></div>
-          </div> 
-         </div>
-        </form>
         
-        
-        
+            
+    </form>
+
+
+
     );
 }
 
